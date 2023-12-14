cd33024ff6bdd93c828b87b5b66fd33dd409b185
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-12-14 00:07:54 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -3,7 +3,7 @@ USE meu_banco;
 
 -- Tabela info_pessoais_funcionario
 CREATE TABLE info_pessoais_funcionario (
-	cpf VARCHAR(15) PRIMARY KEY,
+    cpf VARCHAR(15) PRIMARY KEY,
     nome VARCHAR(40) NOT NULL,
     tel_celular VARCHAR(15) NOT NULL,
     tel_residencial VARCHAR(15),
@@ -13,7 +13,7 @@ CREATE TABLE info_pessoais_funcionario (
 
 -- Tabela departamento
 CREATE TABLE departamento (
-	codigo_depto BIGINT AUTO_INCREMENT PRIMARY KEY,
+    codigo_depto BIGINT AUTO_INCREMENT PRIMARY KEY,
     sigla_depto VARCHAR(15) NOT NULL,
     nome_depto VARCHAR(50) NOT NULL,
     descricao TEXT NOT NULL,
@@ -23,26 +23,26 @@ CREATE TABLE departamento (
 
 -- Tabela funcionario
 CREATE TABLE funcionario (
-	codigo_funcionario BIGINT AUTO_INCREMENT PRIMARY KEY,
+    codigo_funcionario BIGINT AUTO_INCREMENT PRIMARY KEY,
     cargo VARCHAR(100) NOT NULL,
     data_admissao DATE NOT NULL,
     email_funcionario VARCHAR(40) NOT NULL,
     data_demissao DATE,
     cpf_funcionario VARCHAR(15) UNIQUE NOT NULL,
     codigo_depto_funcionario BIGINT,
-	status_funcionario BOOL DEFAULT 1,
-	CONSTRAINT fk_cpf_funcionario FOREIGN KEY (cpf_funcionario) REFERENCES info_pessoais_funcionario(cpf),
+    status_funcionario BOOL DEFAULT 1,
+    CONSTRAINT fk_cpf_funcionario FOREIGN KEY (cpf_funcionario) REFERENCES info_pessoais_funcionario(cpf),
     CONSTRAINT fk_codigo_depto_funcionario FOREIGN KEY (codigo_depto_funcionario) REFERENCES departamento(codigo_depto)
 );
 
 -- Tabela projeto
 CREATE TABLE projeto (
-	codigo_projeto BIGINT AUTO_INCREMENT PRIMARY KEY,
+    codigo_projeto BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome_projeto VARCHAR(50) NOT NULL,
     duracao_horas_projeto INT NOT NULL,
     descricao_depto TEXT NOT NULL,
     codigo_depto_projeto BIGINT,
-	status_projeto BOOL DEFAULT 1,
+    status_projeto BOOL DEFAULT 1,
     CONSTRAINT fk_codigo_depto_projeto FOREIGN KEY (codigo_depto_projeto) REFERENCES departamento(codigo_depto)
 );
 
